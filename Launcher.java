c2dfcba2939504171bce19f4e14aa5222bf63d97
hghazi24 <hassanghazi247@gmail.com>
2026-04-11 11:59:21 +0500

    Add files via upload

---
+++ Launcher.java
@@ -0,0 +1,38 @@
+public class Launcher {
+
+  private double InitVelocity;
+  private double Gravity;
+  private double Angle;
+
+    public Launcher(double InitVelocity, double Gravity, double Angle){
+
+        this.InitVelocity = InitVelocity;
+        this.Gravity = Gravity;
+        this.Angle = Angle;
+    }
+
+    public double getInitVelocity(){
+        return InitVelocity;
+    }
+    public double getGravity(){
+        return Gravity;
+    }
+    public double getAngle(){
+        return Angle;
+    }
+
+    public void setInitVelocity( double iv){
+        this.InitVelocity = iv;
+    }
+    public void setGravity( double g){
+        this.Gravity = g;
+    }
+    public void setAngle( double a){
+        this.Angle = a;
+    }
+
+
+
+
+
+}
