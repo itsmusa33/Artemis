7d462c3f3cde668bad7b909e45584891669a61c1
hghazi24 <hassanghazi247@gmail.com>
2026-04-27 00:24:35 +0500

    Add files via upload

---
+++ src/main/java/com.artemis/ColorScheme.java
@@ -0,0 +1,25 @@
+public class ColorScheme{
+
+    //Colours for background
+    public static final String CANVAS_BG      = "#0a1520";
+    public static final String PLOT_BG        = "#0d1f30";
+    public static final String CONTROL_BG     = "#0d1b2a";
+
+    //Colours for grid
+    public static final String GRID_LINES     = "#1a3050";
+    public static final String GRID_AXES      = "#2a5070";
+    public static final String GRID_LABELS    = "#4a7090";
+    public static final String AXIS_TITLES    = "#6890a8";
+
+    //Colours for trajectory
+    public static final String APEX_DOT       = "#ffdd66";
+    public static final String LANDING_DOT    = "#ff6655";
+    public static final String LAUNCH_DOT     = "#66ff99";
+
+    //Colours for angle arrow
+    public static final String ANGLE_ARROW    = "#4db8ffaa";
+    public static final String ANGLE_ARC      = "#4db8ff66";
+    public static final String ANGLE_LABEL    = "#4db8ff";
+
+    private ColorScheme(){}
+}
