1b6109db8beed3593e8008d068a33373b8bf0c5b
hghazi24 <hassanghazi247@gmail.com>
2026-04-27 00:10:52 +0500

    Update BasicPhysics.java

--- src/main/java/com.artemis/BasicPhysics.java
+++ src/main/java/com.artemis/BasicPhysics.java
@@ -1,36 +1,38 @@
-public interface PhysicsEngine {
+public class BasicPhysics implements PhysicsEngine{
 
-    void computeStep(Projectile p, double dt);
-    double totalFlightTime(Launcher lau);
-    double getMaxRange(Launcher lau);
-    double getMaxHeight(Launcher lau);
-}
 
-public class BasicPhysics implements PhysicsEngine {
-
-    public void computeStep(Projectile p, double dt) {
-
+    //Moves projectile and checks if it has landed
+    @Override
+    public void computeStep(Projectile p, double dt){
         p.setX(p.getX() + p.getVx() * dt);
         p.setY(p.getY() + p.getVy() * dt);
-
         p.setVy(p.getVy() - p.getGravity() * dt);
 
-        p.setTime(p.getTime() + dt);
+        if (p.getY() <= 0 && (p.getVx() != 0 || p.getVy() != 0)){
+            p.setY(0);
+            p.setHasLanded(true);
+        }
     }
+
+    //FLight time calc
     @Override
-    public double totalFlightTime(Launcher lau) {
+    public double getFlightTime(Launcher lau){
         double angleRad = Math.toRadians(lau.getAngle());
         double vy = lau.getInitialVelocity() * Math.sin(angleRad);
         return (2 * vy) / lau.getGravity();
     }
+
+    //Max range calc
     @Override
-    public double getMaxRange(Launcher lau) {
+    public double getMaxRange(Launcher lau){
         double angleRad = Math.toRadians(lau.getAngle());
         double v = lau.getInitialVelocity();
         return (v * v * Math.sin(2 * angleRad)) / lau.getGravity();
     }
+
+    //Max height calc
     @Override
-    public double getMaxHeight(Launcher lau) {
+    public double getMaxHeight(Launcher lau){
         double angleRad = Math.toRadians(lau.getAngle());
         double vy = lau.getInitialVelocity() * Math.sin(angleRad);
         return (vy * vy) / (2 * lau.getGravity());
