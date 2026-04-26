d883a99eca4ea126f8f648a3b8aae10d333004e0
Muhammad Musa <musa20042001@gmail.com>
2026-04-27 00:29:00 +0500

    crater

---
+++ src/main/java/com.artemis/Crater.java
@@ -0,0 +1,17 @@
+public class Crater{
+
+    private final double X;
+    private final double Width;
+
+    public Crater(double X, double Width){
+        this.X  = X;
+        this.Width = Width;
+    }
+
+    public double getX(){
+        return X;
+    }
+    public double getWidth(){
+        return Width;
+    }
+}
