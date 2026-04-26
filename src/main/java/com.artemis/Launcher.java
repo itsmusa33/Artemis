d378574c59259316c5f43c248d310159c854905c
hghazi24 <hassanghazi247@gmail.com>
2026-04-27 00:12:24 +0500

    Update Launcher.java

--- src/main/java/com.artemis/Launcher.java
+++ src/main/java/com.artemis/Launcher.java
@@ -1,38 +1,48 @@
-public class Launcher {
+public class Launcher{
 
-  private double InitVelocity;
-  private double Gravity;
-  private double Angle;
+    private double X;
+    private double Y;
+    private double InitialVelocity;
+    private double Gravity;
+    private double Angle;
+    private double Wind = 0.0;
 
-    public Launcher(double InitVelocity, double Gravity, double Angle){
-
-        this.InitVelocity = InitVelocity;
-        this.Gravity = Gravity;
+    public Launcher(double X, double Y, double Angle, double InitialVelocity){
+        this.X = X;
+        this.Y = Y;
         this.Angle = Angle;
+        this.InitialVelocity = InitialVelocity;
+        this.Gravity = 9.81;
     }
 
+    public double getX(){ 
+        return X; 
+    }
+    public double getY(){ 
+        return Y; 
+    }
+    public double getAngle(){ 
+        return Angle; 
+    }
     public double getInitialVelocity(){
-        return InitVelocity;
+        return InitialVelocity;
     }
     public double getGravity(){
-        return Gravity;
+        return Gravity; 
     }
-    public double getAngle(){
-        return Angle;
+    public double getWind(){
+        return Wind; 
     }
-
-    public void setInitialVelocity( double iv){
-        this.InitVelocity = iv;
+    public void setAngle(double a){ 
+        this.Angle = a; 
     }
-    public void setGravity( double g){
-        this.Gravity = g;
+    public void setInitialVelocity(double v){
+        this.InitialVelocity = v;
     }
-    public void setAngle( double a){
-        this.Angle = a;
+    public void setGravity(double g){
+        this.Gravity = g; 
     }
-
-
-
-
-
+    public void setWind(double w){ 
+        this.Wind = w; 
+    }
 }
